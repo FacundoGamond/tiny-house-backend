28110e23d61af65e4f245a0a439d4db8519d170c
Facundo Gamond <facugamond@hotmail.com>
2021-03-30 23:47:47 -0300

    change

--- server.js
+++ server.js
@@ -3,7 +3,7 @@
 //Conexion a base de datos
 var mongoose = require('mongoose');
 var app = require('./app'); //Configuracion de express
-var port = 3000;
+var port = 7000;
 
 app.listen(port, () => {
     console.log("Servidor corriendo correctamente en la url: http://localhost:"+port);
